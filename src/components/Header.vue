74fe95a54e63c4af7ca606420e8837f24208b3a2
seo-png <iseohyeon128@gmail.com>
2022-01-19 17:29:38 +0900

    Header - nav

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <div class="nav">
+      <div class="nav-item">
+        <RouterLink class="nav-link">
+          Search
+        </RouterLink>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navigations: [
+        {
+          name: 'Search',
+          href: '/'
+        },
+        {
+          name: 'Movie',
+          href: '/movie'
+        },
+        {
+          name: 'About',
+          href: '/about'
+        }
+      ]
+    }
+  },
+}
+</script>
