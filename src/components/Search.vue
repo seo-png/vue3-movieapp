159c065b784bef9a33c57760d7d94bcfcaa6d09d
seo-png <iseohyeon128@gmail.com>
2022-01-23 01:53:28 +0900

    Search - 필터 스타일 적용

---
+++ src/components/Search.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <input 
+      v-model="title"
+      class="form-control"
+      type="text"
+      placeholder="Search for Movies, Series & more" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+      type: 'movie',
+      number: 10,
+      year: '',
+      filters: [
+        {
+          name: 'type',
+          items: ['movie', 'series', 'episode']
+        },
+        {
+          name: 'number',
+          items: [10, 20, 30]
+        },
+        {
+          name: 'year',
+          items: (() => {
+            const years = []
+            for (let i = 2021; i >= 1985; i-=1)
+            return []
+          })()
+        }
+      ]
+    }
+  },
+}
+</script>
