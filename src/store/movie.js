eb08bbefff9522b9f24a9a8cc963a358db201716
seo-png <iseohyeon128@gmail.com>
2022-02-11 00:48:08 +0900

    add error message

--- src/store/movie.js
+++ src/store/movie.js
@@ -27,35 +27,42 @@ export default {
   // 비동기
   actions: {
     async searchMovies({ state, commit }, payload) {
-      const res = await _fetchMovie({
-        ...payload,
-        page: 1
-      })
-      const { Search, totalResults } = res.data
-      commit('updateState',{
-        movies: _uniqBy(Search, 'imdbID')
-      })
-      console.log(totalResults) //268
-      console.log(typeof totalResults) //string
-
-      const total = parseInt(totalResults, 10)
-      const pageLength = Math.ceil(total / 10)
-
-      //추가 요청!
-      if (pageLength > 1) {
-        for (let page = 2; page <= pageLength; page +=1) {
-          if (page > (payload.number /10)) break
-          const res = await _fetchMovie({
-            ...payload,
-            page
-          })
-          const { Search } = res.data
-          commit('updateState', {
-            movies: [
-              ...state.movies, 
-              ..._uniqBy(Search, 'imdbID')]
-          })
+      try {
+        const res = await _fetchMovie({
+          ...payload,
+          page: 1
+        })
+        const { Search, totalResults } = res.data
+        commit('updateState',{
+          movies: _uniqBy(Search, 'imdbID')
+        })
+        console.log(totalResults) //268
+        console.log(typeof totalResults) //string
+  
+        const total = parseInt(totalResults, 10)
+        const pageLength = Math.ceil(total / 10)
+  
+        //추가 요청!
+        if (pageLength > 1) {
+          for (let page = 2; page <= pageLength; page +=1) {
+            if (page > (payload.number /10)) break
+            const res = await _fetchMovie({
+              ...payload,
+              page
+            })
+            const { Search } = res.data
+            commit('updateState', {
+              movies: [
+                ...state.movies, 
+                ..._uniqBy(Search, 'imdbID')]
+            })
+          }
         }
+      } catch(message) {
+        commit('updateState', {
+          movies: [],
+          message
+        })
       }
     }
   }
@@ -64,11 +71,16 @@ export default {
 function _fetchMovie(payload) {
   const { title, type, year, page } = payload
   const OMDB_API_KEY = '7035c60c'
-  const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+  //const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+  const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}`
 
   return new Promise((resolve, reject) => {
     axios.get(url)
       .then(res => {
+        console.log(res)
+        if (res.data.Error) {
+          reject(res.data.Error)
+        }
         resolve(res)
       })
       .catch(err => {
