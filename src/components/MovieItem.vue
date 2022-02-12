6dc350acf20b101dc0e30cc2dd4f364874cb7d72
seo-png <iseohyeon128@gmail.com>
2022-02-13 04:04:56 +0900

    Container 너비 사용자 지정

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -37,6 +37,15 @@ export default {
   background-size: cover;
   overflow: hidden;
   position: relative;
+  &:hover::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    border: 6px solid $primary;
+  }
   .info {
     background-color: rgba($black, .3);
     width: 100%;
