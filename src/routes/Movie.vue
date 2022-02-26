4e9d9ea4c6a04e8c029a84c99e108a2b142254ab
seo-png <iseohyeon128@gmail.com>
2022-02-27 02:07:28 +0900

    고해상도 이미지 출력

--- src/routes/Movie.vue
+++ src/routes/Movie.vue
@@ -21,7 +21,7 @@
       v-else 
       class="movie-details">
       <div 
-        :style="{ backgroundImage: `url(${theMovie.Poster})` }"
+        :style="{ backgroundImage: `url(${requestDiffSizeImage(theMovie.Poster)})` }"
         class="poster"></div>
       <div class="specs">
         <div class="title">
@@ -90,6 +90,11 @@ export default {
       //movie/tt123762
       id: this.$route.params.id
     })
+  },
+  methods: {
+    requestDiffSizeImage(url, size = 700) {
+      return url.replace('SX300', `SX${size}`)
+    }
   }
 }
 </script>
