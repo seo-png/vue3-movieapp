b571f380985b60e447d92d7c5497b6eba2e9f294
seo-png <iseohyeon128@gmail.com>
2022-03-01 22:39:07 +0900

    Nav 경로 일치 및 활성화

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,6 +9,7 @@
         <RouterLink 
           :to="nav.href"
           active-class="active"
+          :class="{ active: isMatch(nav.path) }"
           class="nav-link">
           {{ nav.name }}
         </RouterLink>
@@ -32,7 +33,8 @@ export default {
         },
         {
           name: 'Movie',
-          href: '/movie/tt4520988'
+          href: '/movie/tt4520988',
+          path: /^\/movie/ //'movie'
         },
         {
           name: 'About',
@@ -41,6 +43,12 @@ export default {
       ]
     }
   },
+  methods: {
+    isMatch(path) {
+      if (!path) return false
+      return path.test(this.$route.fullPath)
+    }
+  }
 }
 </script>
 
