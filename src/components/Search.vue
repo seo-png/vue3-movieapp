282647dd59068c0b2b140fdf25f0824dc6b82676
seo-png <iseohyeon128@gmail.com>
2022-03-03 20:20:01 +0900

    부트스트랩 breakpoint 반응형

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -78,6 +78,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~/scss/main";
+
 .container {
   display: flex;
   > * {
@@ -102,6 +104,24 @@ export default {
     height: 50px;
     font-weight: 700;
     flex-shrink: 0;
+  }
+
+  @include media-breakpoint-down(lg) {
+    display: block;
+    input {
+      margin-right: 0;
+      margin-bottom: 10px;
+    }
+    .selects {
+      margin-right: 0;
+      margin-bottom: 10px;
+    }
+    select {
+      width: 100%;
+    }
+  }
+  .btn {
+    width: 100%;
   }
 }
 </style>
