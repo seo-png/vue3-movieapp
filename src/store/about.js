6c3ca7250d0edd49130f9477d1ab0f859900e19a
seo-png <iseohyeon128@gmail.com>
2022-04-19 09:24:54 +0900

    image change

--- src/store/about.js
+++ src/store/about.js
@@ -5,6 +5,6 @@ export default {
     email: 'zoo7733@naver.com',
     blog: 'https://github.com/seo-png',
     phone: '+82-10-9401-2858',
-    image: 'https://heropy.blog/css/images/logo.png'
+    image: 'https://raw.githubusercontent.com/seo-png/vue3-movieapp/master/src/assets/profile.png'
   })
 }
